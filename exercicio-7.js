b71f66f0dedd9e45be72df31e4a5fa6e400466b1
warlei123 <warlei.o.santoss@gmail.com>
2021-04-07 15:53:29 -0700

    Formatacao e alteracao do texto exibido

--- exercicio-7.js
+++ exercicio-7.js
@@ -1,10 +1,13 @@
 var readlineSync = require('readline-sync');
 
 var textoInserido = readlineSync.question('Informe um numero: ');
-var vetor = parseInt(numero)
+var numeroInserido = parseInt(textoInserido)
+
+console.log(`A tabuada de ${numeroInserido} é: `)
 
 for (let i = 1; i <= 12; i++) {
-    let result = i * vetor;
+    let resultado = i * numeroInserido;
 
-    console.log(`${vetor} * ${i} = ${result}`);
+    
+    console.log(`${numeroInserido} * ${i} = ${resultado}`);
 }

759ae47e46fc581f170ab4413d767dc2a9c35a43
warlei123 <warlei.o.santoss@gmail.com>
2021-04-07 10:41:39 -0700

    Adicionado validacao de numero inserido no exercico 4.

--- exercicio-4.js
+++ exercicio-4.js
@@ -1,15 +1,13 @@
-
 var readlineSync = require('readline-sync');
-// enquanto nao passar um numero valido, não para de pedir o numero
+
 var textoInserido = readlineSync.question('Informe um numero: ');
-// validar se o que foi digitado é um numero
 var numeroInserido = parseInt(textoInserido);
 
 while (isNaN(numeroInserido)) {
-    var textoInserido = readlineSync.question('isto não é um numero, por favor, Informe um numero: ');
-    var numeroInserido = parseInt(textoInserido);
+    textoInserido = readlineSync.question('isto não é um numero, por favor, Informe um numero: ');
+    numeroInserido = parseInt(textoInserido);
 }
 
 var resultado = (numeroInserido * (numeroInserido + 1) / 2);
 
-console.log(`A soma de todos os numeros de 0 e ${numeroInserido} é : ${resultado} `)        
+console.log(`A soma de todos os numeros de 0 e ${numeroInserido} é : ${resultado}`)        

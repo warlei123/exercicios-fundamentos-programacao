d9242bb22b304bd5f9fb3f1532cdb2ab944cb2fa
warlei123 <warlei.o.santoss@gmail.com>
2021-04-07 15:55:50 -0700

    Codigo totalmente refeito de acordo com o enunciado do exrcicio

--- exercicio-8.js
+++ exercicio-8.js
@@ -1,24 +1,14 @@
+for (let numeroPrimo = 0; numeroPrimo >=0; numeroPrimo++) {
+    let resto = 0;
 
-var readlinesync = require('readline-sync');
-var numeroInserido = readlineSync.question('Informe um numero ');;
-var fim = parseInt(numeroInserido)
-
-// fazer exatamente igual ao exercicio
-console.log(`os numeros primos entre ${inicio} e ${fim} são:`);
-
-for (let i = 0; loop <= fim; loop++) {
-    let marcacao = 0;
-
-
-    for (let divisor = 2; divisor < loop; divisor++) {
-        if (loop % divisor == 0) {
-         marcacao = 1;
+    for (let divisor = 2; divisor < numeroPrimo; divisor++) {
+        if (numeroPrimo % divisor == 0) {
+            resto = 1;
             break;
         }
     }
 
-   
-    if (loop > 1 && marcacao == 0) {
-        console.log(loop);
+    if (numeroPrimo > 1 && resto == 0) {
+        console.log(numeroPrimo);
     }
 }

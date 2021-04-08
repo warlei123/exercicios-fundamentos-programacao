0cad2b57a5f7e3795be47e5c78a1e70a01cb864d
warlei123 <warlei.o.santoss@gmail.com>
2021-04-08 16:53:38 -0700

    atualizando formatacao

--- exercicio-2.js
+++ exercicio-2.js
@@ -3,7 +3,9 @@ var readlineSync = require('readline-sync');
 var nomeInserido = readlineSync.question('Qual é o seu nome? ');
 var numeroDigitado = !isNaN(parseInt(nomeInserido));
 
-while(nomeInserido == null || nomeInserido == '' || numeroDigitado)
-   var nomeInserido = readlineSync.question('Nenhum nome inserido, por favor, informe o seu nome '); 
 
-console.log(`Olá ${nomeInserido}.`)
+while (nomeInserido == null || nomeInserido == '' || numeroDigitado == true);
+   console.log('Nenhum nome inserido, por favor, tente novamente '); 
+   
+
+console.log(`Olá ${nomeInserido}.`);
